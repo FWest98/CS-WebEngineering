64cd585821896ed254a3ef05baa29eed11083576
Floris Westerman <floriswesterman@msn.com>
2019-03-20 02:30:41 +0100

    Created minimal example

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
 import repo from "./api/repo.js";
 import login from "./api/login.js";
-import loginComponent from "./components/login.js";
-import repositoryListerComponent from "./components/repositorylister.js";
-import repositoryComponent from "./components/repository.js"
